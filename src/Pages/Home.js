59d4375bef98eb17aebb9a6961046a99e12720b2
Labais <reinitto@gmail.com>
2020-04-30 03:55:21 +0200

    fix homepage reflow

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,26 +1,10 @@
 import React, { lazy, Suspense, useEffect, useState } from "react";
 import ContactForm from "../components/contact/ContactForm";
 import Hero from "../components/Hero";
-// import Pricing from "../components/pricing/PricingList";
-// import { digitalPhotos } from "../content/pricing";
-import { mainBg, contactFormBg } from "../content/backgroundImages";
 import LinksToGallery from "../components/LinksToGallery";
-// import { HorizontalScroll } from "../components/HorizontalScroll";
+import { mainBg, contactFormBg } from "../content/backgroundImages";
 import { createJournalItems } from "../components/createJournalItems";
 const HorizontalScroll = lazy(() => import("../components/HorizontalScroll"));
-
-// let isAnyPartOfElementInViewport = el => {
-//   const rect = el.getBoundingClientRect();
-//   const windowHeight =
-//     window.innerHeight || document.documentElement.clientHeight;
-//   const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-
-//   // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
-//   const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
-//   const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
-
-//   return vertInView && horInView;
-// };
 
 const Home = ({ journals, services }) => {
   let [travelJournals, setTravelJournals] = useState();
--- src/components/LinksToGallery.js
+++ src/components/LinksToGallery.js
@@ -9,7 +9,9 @@ let swapArrayElements = function (arr, indexA, indexB) {
   arr[indexB] = temp;
 };
 
-let SingleLink = ({ link, style = {} }) => {
+let SingleLink = ({ link, dimensions = { w: 450, h: "auto" }, style = {} }) => {
+  let { w, h } = dimensions;
+  let imageWidth = w > 300 ? 450 : 300;
   if (link) {
     let { key, name, thumbnailImage } = link;
     return (
@@ -17,8 +19,8 @@ let SingleLink = ({ link, style = {} }) => {
         <div
           style={{
             position: "relative",
-            width: "100%",
-            height: "100%",
+            width: `${w}px`,
+            height: `${h}px`,
           }}
         >
           <Image
@@ -27,8 +29,9 @@ let SingleLink = ({ link, style = {} }) => {
                 ? thumbnailImage
                 : "images/portfolio/couples/placeholder_klk233"
             }
-            width="300"
-            crop="scale"
+            width={imageWidth}
+            // height={h}
+            crop="fill"
             className="p-3 fitted-image"
             style={{
               width: "100%",
@@ -47,18 +50,25 @@ let SingleLink = ({ link, style = {} }) => {
       </Link>
     );
   } else {
-    return null;
+    return (
+      <div
+        style={{
+          width: `${w}px`,
+          height: `${h}px`,
+        }}
+      ></div>
+    );
   }
 };
 
-let LinkLayout = ({ links, reverse = false }) => {
+let LinkLayout = ({ links, largeWH, smallImageWH, reverse = false }) => {
   if (reverse) {
     return (
       <div className="links-to-gallery-layout">
-        <SingleLink link={links[0]} />
+        <SingleLink link={links[0]} dimensions={largeWH} />
         <div className="two-links">
-          <SingleLink link={links[1]} />
-          <SingleLink link={links[2]} />
+          <SingleLink link={links[1]} dimensions={smallImageWH} />
+          <SingleLink link={links[2]} dimensions={smallImageWH} />
         </div>
       </div>
     );
@@ -66,10 +76,10 @@ let LinkLayout = ({ links, reverse = false }) => {
     return (
       <div className="links-to-gallery-layout">
         <div className="two-links">
-          <SingleLink link={links[0]} />
-          <SingleLink link={links[1]} />
+          <SingleLink link={links[0]} dimensions={smallImageWH} />
+          <SingleLink link={links[1]} dimensions={smallImageWH} />
         </div>
-        <SingleLink link={links[2]} />
+        <SingleLink link={links[2]} dimensions={largeWH} />
       </div>
     );
   }
@@ -89,17 +99,39 @@ export default function LinksToGallery({ services }) {
     swapArrayElements(arr, 1, 3);
   }
 
+  let scrollWidth = document.body.scrollWidth;
+  var actualInnerWidth = Math.ceil(scrollWidth * 0.9);
+  if (width > 768) {
+    actualInnerWidth = 690;
+  }
+  if (width > 992) {
+    actualInnerWidth = 930;
+  }
+  if (width > 1200) {
+    actualInnerWidth = 1110;
+  }
+  let bigImageWH = Math.floor(actualInnerWidth / 2);
+  let smallImageWidth = Math.floor(bigImageWH / 2);
+  let smallImageHeight = Math.ceil(1.4 * smallImageWidth);
   return (
     <div
-      className={width < 768 ? "pb-3 mx-auto w-90 " : "container pb-3"}
+      className={width <= 768 ? "pb-3 mx-auto w-90 " : "container pb-3"}
       style={{
         backgroundColor: "#faf7f6",
       }}
     >
       <h2 className="text-center">Capture Your Life's Journey</h2>
       <CloudinaryContext cloudName="sunshinephoto">
-        {width < 769 ? (
-          <div className="d-flex flex-wrap">
+        {width <= 768 ? (
+          <div
+            className="d-flex flex-wrap"
+            style={{
+              height:
+                width <= 768
+                  ? Math.floor(bigImageWH * 1.4) * 3
+                  : bigImageWH + smallImageHeight,
+            }}
+          >
             {arr.map((link) => (
               <div className="w-50" key={link.key}>
                 <SingleLink
@@ -107,14 +139,27 @@ export default function LinksToGallery({ services }) {
                   style={{
                     width: "100%",
                   }}
+                  dimensions={{
+                    w: bigImageWH,
+                    h: Math.floor(bigImageWH * 1.4),
+                  }}
                 />
               </div>
             ))}
           </div>
         ) : (
           <div className="d-flex links-to-gallery">
-            <LinkLayout links={arr.slice(0, 3)} />
-            <LinkLayout links={arr.slice(3)} reverse={true} />
+            <LinkLayout
+              links={arr.slice(0, 3)}
+              largeWH={{ w: bigImageWH, h: bigImageWH }}
+              smallImageWH={{ w: smallImageWidth, h: smallImageHeight }}
+            />
+            <LinkLayout
+              links={arr.slice(3)}
+              reverse={true}
+              largeWH={{ w: bigImageWH, h: bigImageWH }}
+              smallImageWH={{ w: smallImageWidth, h: smallImageHeight }}
+            />
           </div>
         )}
       </CloudinaryContext>
