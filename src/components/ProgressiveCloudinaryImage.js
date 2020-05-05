2428c996886379605fd32e70d2e3be9a2beb0cb1
Labais <reinitto@gmail.com>
2020-05-05 10:47:09 +0200

    load images when in viewport

--- src/components/ProgressiveCloudinaryImage.js
+++ src/components/ProgressiveCloudinaryImage.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Image, Transformation } from "cloudinary-react";
 export const ProgressiveCloudinaryImage = (
   { publicId, style = {} },
   ...rest
 ) => {
+  let imageRef = useRef();
+  let [imagePublicId, setImagePublicId] = useState();
+  useEffect(() => {
+    let observer;
+    let theElement;
+    let didCancel = false;
+    if (imageRef.current.element && imagePublicId !== publicId) {
+      if (IntersectionObserver) {
+        observer = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              if (
+                !didCancel &&
+                (entry.intersectionRatio > 0 || entry.isIntersecting)
+              ) {
+                setImagePublicId(publicId);
+                observer.unobserve(imageRef.current.element);
+              }
+            });
+          },
+          {
+            threshold: 0.1,
+            rootMargin: "10%",
+          }
+        );
+        theElement = imageRef.current.element;
+        observer.observe(imageRef.current.element);
+      } else {
+        // Old browsers fallback
+        setImagePublicId(publicId);
+      }
+    }
+    return () => {
+      didCancel = true;
+      // on component cleanup, we remove the listner
+      if (observer && observer.unobserve) {
+        observer.unobserve(theElement);
+      }
+    };
+  }, [publicId, imagePublicId]);
   return (
     <Image
+      ref={imageRef}
       dpr="auto"
       responsive
       width="auto"
       crop="scale"
       responsiveUseBreakpoints="true"
-      publicId={publicId}
+      publicId={imagePublicId || ""}
       secure="true"
       style={{
         width: "100%",
