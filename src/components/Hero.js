e2fb17cc27864bec9a69ab230e2159f9439cb8a4
Labais <reinitto@gmail.com>
2020-05-08 06:00:20 +0200

    fix hero layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,25 +10,12 @@ let placeholderColor = "#b4c7d4";
 
 export default function Hero({ title, subtitle, keywords, height = "100vh" }) {
   let windowWidth = useWindowWidth();
-  let titleSize = "8vw";
   let subtitleSize = windowWidth * 0.024 > 30 ? "30px" : "2.4vw";
-  let keywordPadding = "0.2rem 3rem";
-  let keywordLetterSpacing = "1.22rem";
-  let subtitleSpacing = "1.34rem";
   let keywordSize = windowWidth * 0.01 > 16 ? "1rem" : "10px";
-  let color = "white";
-  if (windowWidth * 0.08 < 65) {
-    titleSize = "65px";
-    color = "#3d4856";
-    keywordPadding = "0.2rem 1rem";
-    keywordLetterSpacing = "0.3rem";
-    subtitleSpacing = "0.8rem";
-  }
+
   return (
-    // <CloudinaryContext cloudName="sunshinephoto">
     <LazyBackground
       src={mainBg}
-      // placeholder={placeholderBg}
       className="cover-image"
       style={{
         width: "100%",
@@ -37,27 +24,17 @@ export default function Hero({ title, subtitle, keywords, height = "100vh" }) {
       }}
     >
       <div
-        // className="cover-image"
         style={{
           width: "100%",
           height,
         }}
       >
         <div className="hero">
-          <h1
-            className="text-center title-text"
-            style={{
-              fontSize: titleSize,
-              color,
-            }}
-          >
-            {title}
-          </h1>
+          <h1 className="text-center title-text">{title}</h1>
           <h2
             className="text-center text-uppercase hero-subtitle"
             style={{
               fontSize: subtitleSize,
-              letterSpacing: subtitleSpacing,
             }}
           >
             {subtitle}
@@ -70,8 +47,6 @@ export default function Hero({ title, subtitle, keywords, height = "100vh" }) {
                       className="hero-keyword text-uppercase"
                       key={i}
                       style={{
-                        padding: keywordPadding,
-                        letterSpacing: keywordLetterSpacing,
                         fontSize: keywordSize,
                       }}
                     >
@@ -95,7 +70,6 @@ export default function Hero({ title, subtitle, keywords, height = "100vh" }) {
         </div>
       </div>
     </LazyBackground>
-    // </CloudinaryContext>
   );
 }
 Hero.defaultProps = {
