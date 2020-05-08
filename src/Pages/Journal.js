db93e3a6b0137a9fd07ea80f80d1bbdb4e196659
Labais <reinitto@gmail.com>
2020-05-08 06:47:10 +0200

    add dynamic meta tags

--- src/Pages/Journal.js
+++ src/Pages/Journal.js
@@ -1,19 +1,37 @@
 import React, { Fragment, Suspense, useEffect, useState } from "react";
 import ImageGalleryWithoutLighbox from "../components/portfolio/ImageGalleryWithoutLighbox";
 import IntroImage from "../components/IntroImage";
+import MetaTags from "react-meta-tags";
 import Spinner from "../components/Spinner";
 
 export default function Journal({ journals }) {
   let [journal, setJournal] = useState(null);
+  let [description, setDescription] = useState();
   let journalId = window.location.pathname.replace(`/journal/`, "");
   useEffect(() => {
-    if (journals) {
+    if (journals && Object.keys(journals).length > 0) {
       setJournal(journals[journalId]);
+      let desLength = 0;
+      let des = "";
+      Object.keys(journals[journalId].images).forEach((key) => {
+        if (journals[journalId].images[key].text.length > desLength) {
+          des = journals[journalId].images[key].text;
+          desLength = journals[journalId].images[key].text.length;
+        }
+      });
+      setDescription(des);
     }
+    return () => {
+      setJournal(null);
+      setDescription(null);
+    };
   }, [journalId, journals]);
-
   return journal ? (
     <Fragment>
+      <MetaTags id={journalId}>
+        <title>{journal.title.title}</title>
+        <meta name="description" content={description} />
+      </MetaTags>
       <Suspense fallback={<div style={{ height: "400px" }}></div>}>
         <IntroImage
           subtitle={journal ? [journal.title.title] : null}
